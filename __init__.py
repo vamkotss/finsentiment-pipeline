44b8abeb1e2cedc3fa6a6c4c2c810150ee4153d1
vamkotsss <kotasrivamsikrishna@gmail.com>
2026-05-13 15:29:05 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-"""Financial news sentiment pipeline."""
-from .schema import SentimentResult
-from .classifier import classify_headline
-from .batch import process_batch
-
-__all__ = ["SentimentResult", "classify_headline", "process_batch"]
